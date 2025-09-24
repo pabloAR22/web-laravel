99d58db2cf22fe60e2066d499c013efa8fb212df
Pablo <radabeca23gmail.com>
2025-09-23 22:11:06 -0500

    view commnets and answers

---
+++ resources/views/components/forum/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="flex items-center justify-between h-16">
+    <div>
+        <a href="{{ route('home') }}">
+            <x-forum.logo />
+        </a>
+    </div>
+
+    <div class="flex gap-4">
+        <a href="" class="text-sm font-semibold">Foro</a>
+        <a href="" class="text-sm font-semibold">Blog</a>
+    </div>
+
+    <div class="">
+        <a href="" class="text-sm font-semibold">
+            Log in &rarr;
+        </a>
+    </div>
+</nav>
